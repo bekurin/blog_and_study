9bbb383801a9d9d8cece4d23719f59bbc0424fb4
chungchung <61677941+piaochung@users.noreply.github.com>
2021-08-14 18:08:33 +0900

    configure main page using mustache

---
+++ src/test/java/com/example/springboot/web/IndexControllerTest.java
@@ -0,0 +1,26 @@
+package com.example.springboot.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class IndexControllerTest {
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void loading_main_page(){
+        //when
+        String body = this.restTemplate.getForObject("/", String.class);
+
+        //then
+        assertThat(body).contains("스프링 부트로 시작하는 웹사이트");
+    }
+}
