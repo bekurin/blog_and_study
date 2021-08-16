57f405f9e6da5519f361d7e2dcae0c25b74de73c
chungchung <61677941+piaochung@users.noreply.github.com>
2021-08-16 10:29:28 +0900

    implement login function with Spring Security and OAuth 2.0

--- src/main/java/com/example/springboot/web/IndexController.java
+++ src/main/java/com/example/springboot/web/IndexController.java
@@ -1,13 +1,43 @@
 package com.example.springboot.web;
 
+import com.example.springboot.config.auth.LoginUser;
+import com.example.springboot.config.auth.dto.SessionUser;
+import com.example.springboot.service.posts.PostsService;
+import com.example.springboot.web.dto.PostsResponseDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
+    private final HttpSession httpSession;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model, @LoginUser SessionUser user) {
+        model.addAttribute("posts", postsService.findAllDesc());
+
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model){
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+        return "posts-update";
     }
 }
