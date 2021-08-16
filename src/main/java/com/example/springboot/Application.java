3ac4d3ad986f7788f763f385a527377bf1b6cc99
chungchung <61677941+piaochung@users.noreply.github.com>
2021-08-16 10:56:30 +0900

    update Application.java

--- src/main/java/com/example/springboot/Application.java
+++ src/main/java/com/example/springboot/Application.java
@@ -9,5 +9,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+
     }
 }
