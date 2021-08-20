2ec35519cc5b4e338869c4dca16fe9bdfadb5d6a
chungchung <61677941+piaochung@users.noreply.github.com>
2021-08-20 14:07:14 +0900

    4. 회원 기능 테스트

--- Spring-Boot/jpashop/jpashop/src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ Spring-Boot/jpashop/jpashop/src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -2,22 +2,26 @@ package jpabook.jpashop.service;
 
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.repository.MemberRepository;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
-@ExtendWith(SpringExtension.class)
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 @Transactional
-class MemberServiceTest {
+public class MemberServiceTest {
 
-    @Autowired MemberService memberService;
-    @Autowired MemberRepository memberRepository;
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
 
     @Test
     public void 회원가입() throws Exception {
@@ -31,13 +35,21 @@ class MemberServiceTest {
         //then
         assertEquals(member, memberRepository.findOne(savedId));
     }
-    
-    @Test
+
+    @Test(expected = IllegalStateException.class)
     public void 중복_회원_예외() throws Exception {
         //given
-        
+        Member member1 = new Member();
+        member1.setName("test1");
+
+        Member member2 = new Member();
+        member2.setName("test1");
+
         //when
-        
+        memberService.join(member1);
+        memberService.join(member2); //예외가 발생해야 한다.
+
         //then
+        fail("예외가 발생해야 한다.");
     }
 }
