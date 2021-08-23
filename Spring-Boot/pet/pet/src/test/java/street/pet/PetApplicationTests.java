2431d89d904242ebeb5ccbd9dbfdb1a08cec6dd4
chungchung <61677941+piaochung@users.noreply.github.com>
2021-08-23 20:03:49 +0900

    initial commit

---
+++ Spring-Boot/pet/pet/src/test/java/street/pet/PetApplicationTests.java
@@ -0,0 +1,13 @@
+package street.pet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
