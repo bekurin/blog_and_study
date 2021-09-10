018912614411d7f3533d674ecbd64e9e56288aa5
chungchung <61677941+piaochung@users.noreply.github.com>
2021-09-10 13:55:09 +0900

    API 테스트 및 Spring Rest Docs 적용

--- Spring-Boot/pet/pet/src/main/java/street/pet/web/api/PetApiController.java
+++ Spring-Boot/pet/pet/src/main/java/street/pet/web/api/PetApiController.java
@@ -2,6 +2,7 @@ package street.pet.web.api;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import street.pet.domain.Pet;
@@ -28,7 +29,7 @@ public class PetApiController extends BaseApiController{
 
     @GetMapping("/api/v1/pet")
     public PetResponseDto petV1(
-            @RequestParam(name = "id", defaultValue = "0") Long id){
+            @RequestParam(value = "id") Long id){
         Pet pet = petRepository.findOne(id);
         return new PetResponseDto(pet);
     }
---
+++ Spring-Boot/pet/pet/src/test/java/street/pet/web/api/MemberApiControllerTest.java
@@ -0,0 +1,35 @@
+package street.pet.web.api;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.request;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class MemberApiControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    public void member() throws Exception {
+        mockMvc.perform(get("/api/v1/members/"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+}
