b2d8d47b9886e5277fc3827f77a0ac3dce3bc701
chungchung <61677941+piaochung@users.noreply.github.com>
2021-09-15 11:17:05 +0900

    initial commit

---
+++ Spring-Boot/hello-batch/hello-batch/src/main/java/io/spring/batch/hellobatch/HelloBatchApplication.java
@@ -0,0 +1,13 @@
+package io.spring.batch.hellobatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloBatchApplication.class, args);
+	}
+
+}
---
+++ Spring-Boot/hello-batch/hello-batch/src/test/java/io/spring/batch/hellobatch/HelloBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package io.spring.batch.hellobatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
