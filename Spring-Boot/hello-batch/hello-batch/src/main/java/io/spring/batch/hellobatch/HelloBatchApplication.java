0132962907c7758b6c8c7c1e30aab7240e57b1bf
chungchung <61677941+piaochung@users.noreply.github.com>
2021-09-18 17:27:27 +0900

    4장-잡과 스텝 이해하기

--- Spring-Boot/hello-batch/hello-batch/src/main/java/io/spring/batch/hellobatch/HelloBatchApplication.java
+++ Spring-Boot/hello-batch/hello-batch/src/main/java/io/spring/batch/hellobatch/HelloBatchApplication.java
@@ -6,10 +6,12 @@ import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -27,17 +29,25 @@ public class HelloBatchApplication {
 	@Bean
 	public Job job() {
 		return this.jobBuilderFactory.get("basicJob")
-				.start(step())
+				.start(step1())
 				.build();
 	}
 
 	@Bean
-	public Step step() {
+	public Step step1() {
 		return this.stepBuilderFactory.get("step1")
-				.tasklet(((contribution, chunkContext) -> {
-					System.out.println("Hello, Batch!");
-					return RepeatStatus.FINISHED;
-				})).build();
+				.tasklet(helloWorldTasklet(null))
+				.build();
+	}
+
+	@StepScope
+	@Bean
+	public Tasklet helloWorldTasklet(
+			@Value("#{jobParameters['name']}") String name) {
+		return ((contribution, chunkContext) -> {
+			System.out.println("Hello!" + name);
+			return RepeatStatus.FINISHED;
+		});
 	}
 
 	public static void main(String[] args) {
