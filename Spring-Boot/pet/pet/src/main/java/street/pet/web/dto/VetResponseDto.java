6dfe64e3c3ff2f7dcd2e98c41ca6b3bd5b8d5c56
chungchung <61677941+piaochung@users.noreply.github.com>
2021-09-18 23:00:05 +0900

    Feat: "추가 VetResponseDto id field"

--- Spring-Boot/pet/pet/src/main/java/street/pet/web/dto/VetResponseDto.java
+++ Spring-Boot/pet/pet/src/main/java/street/pet/web/dto/VetResponseDto.java
@@ -6,10 +6,12 @@ import street.pet.domain.Vet;
 @Data
 public class VetResponseDto {
 
+    private Long id;
     private String doctor;
     private String departmentName;
 
     public VetResponseDto(Vet entity) {
+        id = entity.getId();
         doctor = entity.getName();
         departmentName = entity.getDepartment().getName();
     }
