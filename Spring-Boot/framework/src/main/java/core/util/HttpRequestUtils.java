46b579152249ecae459a8ea1b6f6993999f411d1
chungchung <61677941+piaochung@users.noreply.github.com>
2021-09-21 18:09:16 +0900

    Feat: "수정 parseCookie separator ';' -> '; '"

--- Spring-Boot/framework/src/main/java/core/util/HttpRequestUtils.java
+++ Spring-Boot/framework/src/main/java/core/util/HttpRequestUtils.java
@@ -13,7 +13,7 @@ public class HttpRequestUtils {
     }
 
     public static Map<String, String> parseCookies(String cookies) {
-        return parseValues(cookies, ";");
+        return parseValues(cookies, "; ");
     }
 
     private static Map<String, String> parseValues(String values, String separator) {
