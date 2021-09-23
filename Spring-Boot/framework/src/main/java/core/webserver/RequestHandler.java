f64ca98e7c09ca1735bb76df2a63d756a597741d
chungchung <61677941+piaochung@users.noreply.github.com>
2021-09-23 18:12:56 +0900

    Refactor: "추가 RequestHandler에서 Controller 역할 분리"

--- Spring-Boot/framework/src/main/java/core/webserver/RequestHandler.java
+++ Spring-Boot/framework/src/main/java/core/webserver/RequestHandler.java
@@ -1,9 +1,6 @@
 package core.webserver;
 
 import core.webserver.controller.Controller;
-import core.webserver.controller.CreateUserController;
-import core.webserver.controller.ListUserController;
-import core.webserver.controller.LoginController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,26 +8,16 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
-import java.util.HashMap;
-import java.util.Map;
 
 public class RequestHandler extends Thread {
 
-    private static final Logger log = LoggerFactory.getLogger(RequestHandlerOld.class);
+    private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
     private Socket connection;
-    private Map<String, Controller> controllers = new HashMap<>();
 
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
-
-        initController();
     }
 
-    private void initController() {
-        controllers.put("/user/create", new CreateUserController());
-        controllers.put("/user/list", new ListUserController());
-        controllers.put("/user/login", new LoginController());
-    }
 
     public void run() {
         log.debug("New Client Connect! Connected IP: {}, Port: {}", connection.getInetAddress(), connection.getPort());
@@ -39,15 +26,13 @@ public class RequestHandler extends Thread {
             HttpResponse response = new HttpResponse(out);
             HttpRequest request = new HttpRequest(in);
 
-            if (request.getPath().contains(".html")) {
-                response.forward(request.getPath());
-            }
+            Controller controller = RequestMapping.getControllers(request.getPath());
 
-            for (String key : controllers.keySet()) {
-                if (request.getPath().equals(key)) {
-                    Controller controller = controllers.get(key);
-                    controller.service(request, response);
-                }
+            if (controller == null) {
+                String path = request.getDefaultPath(request.getPath());
+                response.forward(path);
+            } else {
+                controller.service(request, response);
             }
 
         } catch (IOException e) {
