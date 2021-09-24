1cf4425bfb1e522aeb72bb71f2cc2d22af107e98
chungchung <61677941+piaochung@users.noreply.github.com>
2021-09-24 23:06:46 +0900

    Feat: "추가 Data를 사용하여 JobParameter 구분"

---
+++ Spring-Boot/hello-batch/hello-batch/src/main/java/io/spring/batch/hellobatch/batch/DailyJobTimeStamper.java
@@ -0,0 +1,16 @@
+package io.spring.batch.hellobatch.batch;
+
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.JobParametersIncrementer;
+
+import java.util.Date;
+
+public class DailyJobTimeStamper implements JobParametersIncrementer {
+    @Override
+    public JobParameters getNext(JobParameters parameters) {
+        return new JobParametersBuilder(parameters)
+                .addDate("currentDate", new Date())
+                .toJobParameters();
+    }
+}
