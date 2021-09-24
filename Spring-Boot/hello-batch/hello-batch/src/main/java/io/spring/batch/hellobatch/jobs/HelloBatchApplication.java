15281f744da9012993bbe296aefdaecde358e880
chungchung <61677941+piaochung@users.noreply.github.com>
2021-09-24 23:08:11 +0900

    Feat: "추가 beforeJob, afterJob을 사용하여 Job 이름과 상태 출력"

--- Spring-Boot/hello-batch/hello-batch/src/main/java/io/spring/batch/hellobatch/jobs/HelloBatchApplication.java
+++ Spring-Boot/hello-batch/hello-batch/src/main/java/io/spring/batch/hellobatch/jobs/HelloBatchApplication.java
@@ -1,13 +1,16 @@
-package io.spring.batch.hellobatch;
+package io.spring.batch.hellobatch.jobs;
 
+import io.spring.batch.hellobatch.batch.DailyJobTimeStamper;
+import io.spring.batch.hellobatch.batch.JobLoggerListener;
+import io.spring.batch.hellobatch.batch.ParameterValidator;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepScope;
-import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.job.CompositeJobParametersValidator;
+import org.springframework.batch.core.job.DefaultJobParametersValidator;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +18,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 @EnableBatchProcessing
 @SpringBootApplication
@@ -27,25 +32,47 @@ public class HelloBatchApplication {
 	private StepBuilderFactory stepBuilderFactory;
 
 	@Bean
+	public CompositeJobParametersValidator validator() {
+		CompositeJobParametersValidator validator = new CompositeJobParametersValidator();
+
+		DefaultJobParametersValidator defaultJobParametersValidator = new DefaultJobParametersValidator(
+				new String[]{"fileName"},
+				new String[]{"name", "currentDate"});
+
+		defaultJobParametersValidator.afterPropertiesSet();
+
+		validator.setValidators(
+				Arrays.asList(new ParameterValidator(), defaultJobParametersValidator));
+
+		return validator;
+	}
+
+	@Bean
 	public Job job() {
 		return this.jobBuilderFactory.get("basicJob")
 				.start(step1())
+				.validator(validator())
+				.incrementer(new DailyJobTimeStamper())
+				.listener(new JobLoggerListener())
 				.build();
 	}
 
 	@Bean
 	public Step step1() {
 		return this.stepBuilderFactory.get("step1")
-				.tasklet(helloWorldTasklet(null))
+				.tasklet(helloWorldTasklet(null, null))
 				.build();
 	}
 
 	@StepScope
 	@Bean
 	public Tasklet helloWorldTasklet(
-			@Value("#{jobParameters['name']}") String name) {
+			@Value("#{jobParameters['name']}") String name,
+			@Value("#{jobParameters['fileName']}") String fileName) {
 		return ((contribution, chunkContext) -> {
-			System.out.println("Hello!" + name);
+			System.out.println(String.format("Hello, %s", name));
+			System.out.println(String.format("fileName = %s", fileName));
+
 			return RepeatStatus.FINISHED;
 		});
 	}
