cbff1c97ddb8ca973a9987e475bc5e668c0708fa
chungchung <61677941+piaochung@users.noreply.github.com>
2021-09-26 22:54:42 +0900

    Feat: "추가 스프링 MVC - 기본 기능"

---
+++ Spring-Boot/springmvc/src/main/java/hello/springmvc/basic/LogTestController.java
@@ -0,0 +1,24 @@
+package hello.springmvc.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class LogTestController {
+
+//    private final Logger log = LoggerFactory.getLogger(LogTestController.class);
+
+    @RequestMapping("/log-test")
+    public String logTest() {
+        String name = "Spring";
+
+        log.trace("trace log={}", name);
+        log.debug("debug log={}", name);
+        log.info("info log={}", name);
+        log.warn("warn log={}", name);
+        log.error("error log={}", name);
+        return "ok";
+    }
+}
