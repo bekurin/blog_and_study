d0d9deea7b3e0fea823394d5776fe459cc4e6122
chungchung <61677941+piaochung@users.noreply.github.com>
2021-09-26 17:40:31 +0900

    Feat: "추가 스프링 MVC - 구조 이해 실습 코드"

--- Spring-Boot/servlet/src/main/java/hello/servlet/ServletApplication.java
+++ Spring-Boot/servlet/src/main/java/hello/servlet/ServletApplication.java
@@ -3,6 +3,7 @@ package hello.servlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
 
 @ServletComponentScan
 @SpringBootApplication
