dfb363833739bfabe2437198e243d0f3d691115a
chungchung <61677941+piaochung@users.noreply.github.com>
2022-02-14 19:39:44 +0900

    Feat: "추가 클린코드 한달한권 프로젝트"

---
+++ Spring-Boot/houseUtils/src/main/java/example/houseUtils/HouseUtilsApplication.java
@@ -0,0 +1,13 @@
+package example.houseUtils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseUtilsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HouseUtilsApplication.class, args);
+	}
+
+}
---
+++ Spring-Boot/houseUtils/src/test/java/example/houseUtils/HouseUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package example.houseUtils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseUtilsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
