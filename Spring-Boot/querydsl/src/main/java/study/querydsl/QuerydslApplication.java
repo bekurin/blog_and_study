5b3eb2f163851ce8d3d213300426b61fcd1c87a0
chungchung <61677941+piaochung@users.noreply.github.com>
2022-02-15 21:26:05 +0900

    Feat: "추가 예제 도메인 모델"

--- Spring-Boot/querydsl/src/main/java/study/querydsl/QuerydslApplication.java
+++ Spring-Boot/querydsl/src/main/java/study/querydsl/QuerydslApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class QuerydslApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(QuerydslApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(QuerydslApplication.class, args);
+    }
 }
