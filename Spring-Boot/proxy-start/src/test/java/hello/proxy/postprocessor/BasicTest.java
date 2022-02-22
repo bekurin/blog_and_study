0a3caaa0eda467f6e28845566ddf515eded20cc1
chungchung <61677941+piaochung@users.noreply.github.com>
2022-02-22 17:24:40 +0900

    Feat: "추가 빈 후처리기"

---
+++ Spring-Boot/proxy-start/src/test/java/hello/proxy/postprocessor/BasicTest.java
@@ -0,0 +1,50 @@
+package hello.proxy.postprocessor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BasicTest {
+
+    @Test
+    void basicConfig() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(BasicConfig.class);
+
+        // A는 bean으로 등록
+        A a = ac.getBean("beanA", A.class);
+        a.helloA();
+
+        Assertions.assertThrows(
+                NoSuchBeanDefinitionException.class,
+                () -> ac.getBean("beanB", B.class));
+    }
+
+    @Slf4j
+    @Configuration
+    static class BasicConfig {
+
+        @Bean(name = "beanA")
+        public A a() {
+            return new A();
+        }
+    }
+
+
+    @Slf4j
+    static class A {
+        public void helloA() {
+            log.info("hello A");
+        }
+    }
+
+    @Slf4j
+    static class B {
+        public void helloB() {
+            log.info("hello B");
+        }
+    }
+}
