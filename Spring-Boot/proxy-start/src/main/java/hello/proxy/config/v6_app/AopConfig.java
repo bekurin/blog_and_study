54d2fb78f2a2e60bbe23cfe938601afccd88e5f2
chungchung <61677941+piaochung@users.noreply.github.com>
2022-02-23 17:48:06 +0900

    Feat: "추가 AspectAOP 적용"

---
+++ Spring-Boot/proxy-start/src/main/java/hello/proxy/config/v6_app/AopConfig.java
@@ -0,0 +1,19 @@
+package hello.proxy.config.v6_app;
+
+import hello.proxy.config.AppV1Config;
+import hello.proxy.config.AppV2Config;
+import hello.proxy.config.v6_app.aspect.LogTraceAspect;
+import hello.proxy.trace.logtrace.LogTrace;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@Import({AppV1Config.class, AppV2Config.class})
+public class AopConfig {
+
+    @Bean
+    public LogTraceAspect logTraceAspect(LogTrace logTrace) {
+        return new LogTraceAspect(logTrace);
+    }
+}
