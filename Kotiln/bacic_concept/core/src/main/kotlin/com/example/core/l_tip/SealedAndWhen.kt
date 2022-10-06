159bd39537845130f4e9739000da77f0ea9155cc
chungchung <HangJaStar@gmail.com>
2022-10-06 15:24:03 +0900

    Feat: "추가 sealed 클래스를 사용한 else 분기 없애기"

--- Kotiln/bacic_concept/core/src/main/kotlin/com/example/core/l_tip/SealedAndWhen.kt
+++ Kotiln/bacic_concept/core/src/main/kotlin/com/example/core/l_tip/SealedAndWhen.kt
@@ -26,6 +26,10 @@ fun mapItem(item: Item<*>) = when(item) {
     is Item.NewPencil -> println("id = ${item.content.id}, name = ${item.content.name}, type = ${item.type}")
 }
 
+/**
+ * sealed 클래스를 사용하여 else 분기 처리 하지 않는 법
+ * 조건 처리가 필요할 경우 새로운 타입에 대해 컴파일 에러를 발생시켜주므로 고려해볼만 하다.
+ */
 fun main() {
     val pencil = Pencil(3, "나는 연필")
     val note = Note(4, "나는 공책")
