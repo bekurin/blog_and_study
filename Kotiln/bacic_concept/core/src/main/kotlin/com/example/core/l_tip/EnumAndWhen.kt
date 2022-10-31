3b521a33d0abc03991807317f847e566af8529df
chungchung <blue.glaucus17@gmail.com>
2022-10-31 11:57:34 +0900

    Feat: "추가 enum과 when 분기 처리"

---
+++ Kotiln/bacic_concept/core/src/main/kotlin/com/example/core/l_tip/EnumAndWhen.kt
@@ -0,0 +1,45 @@
+package com.example.core.l_tip
+
+fun kakaoLogin() {
+    println("kakao 로그인 중!")
+}
+
+fun googleLogin() {
+    println("google 로그인 중!")
+}
+
+fun googleAfterLogin() {
+    println("google 로그인 후 처리 작업 실행!")
+}
+
+fun naverLogin() {
+    println("naver 로그인 중!")
+}
+
+fun originLogin(type: String) {
+    if (type == "KAKAO_LOGIN") {
+        kakaoLogin()
+    } else if (type == "GOOGLE_LOGIN") {
+        googleLogin()
+        googleAfterLogin()
+    } else if (type == "NAVER_LOGIN") {
+        naverLogin()
+    }
+}
+
+enum class Type {KAKAO_LOGIN, GOOGLE_LOGIN, NAVER_LOGIN}
+
+fun enumLogin(type: Type) {
+    when(type) {
+        Type.KAKAO_LOGIN -> kakaoLogin()
+        Type.GOOGLE_LOGIN -> {
+            googleLogin()
+            googleAfterLogin()
+        }
+        Type.NAVER_LOGIN -> naverLogin()
+    }
+}
+
+fun main() {
+    originLogin("GOOGLE_LOGIN")
+}
