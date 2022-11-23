11e776d5dbcf14cf911c5e7da60608261749086a
chungchung <bekurin17@gmail.com>
2022-11-23 22:14:28 +0900

    Feat: "추가 array, list 출력 확장 함수"

--- Kotiln/kotlin-cookbook/src/main/kotlin/util/util.kt
+++ Kotiln/kotlin-cookbook/src/main/kotlin/util/util.kt
@@ -1,6 +1,26 @@
 package util
 
 fun printLine(size: Int = 30) {
-    val line =  List(size) {"-"}.joinToString(separator = "")
+    val line = List(size) { "-" }.joinToString(separator = "")
     println(line)
+}
+
+fun <T> Array<T>.printArray(separator: String = ", "): String {
+    var result = ""
+    this.forEachIndexed { index, it ->
+        result +=
+            if (index == (this.size - 1)) it.toString()
+            else (it.toString() + separator)
+    }
+    return result
+}
+
+fun <T> List<T>.printList(separator: String = ", "): String {
+    var result = ""
+    this.forEachIndexed { index, it ->
+        result +=
+            if (index == (this.size - 1)) it.toString()
+            else (it.toString() + separator)
+    }
+    return result
 }
