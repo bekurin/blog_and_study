110cae318b02979a2b42552fd015cc5b7fd07692
chungchung <bekurin17@gmail.com>
2022-12-01 19:19:14 +0900

    Feat: 코틀린 쿡북 9장 테스트

--- Kotiln/kotlin-cookbook/src/test/kotlin/chapter9/BookTests.kt
+++ Kotiln/kotlin-cookbook/src/test/kotlin/chapter9/BookTests.kt
@@ -1,7 +1,7 @@
 package chapter9
 
-import com.sun.org.apache.xerces.internal.util.PropertyState.`is`
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
 import java.time.LocalDate
 import java.time.Month
 import kotlin.test.assertEquals
@@ -24,5 +24,27 @@ class BookTests {
         assertEquals(book.published, LocalDate.of(2013, Month.SEPTEMBER, 30))
     }
 
-    //TODO: 9장 나머지 실습도 추가히기
+    @Test
+    fun `use Junit5 assertAll`() {
+        //given
+        val book = Book("123124", "코틀린 쿡북", "켄 코우젠", LocalDate.of(2013, Month.SEPTEMBER, 30))
+
+        //then
+        assertAll("check all properties of a book",
+            { assertEquals(book.isbn, "123124") },
+            { assertEquals(book.title, "코틀린 쿡북") },
+            { assertEquals(book.author, "켄 코우젠") },
+            { assertEquals(book.published, LocalDate.of(2013, Month.SEPTEMBER, 30)) }
+        )
+    }
+
+    @Test
+    fun `use data class`() {
+        //given
+        val book = Book("123124", "코틀린 쿡북", "켄 코우젠", LocalDate.of(2013, Month.SEPTEMBER, 30))
+        val expectedBook = Book("123124", "코틀린 쿡북", "켄 코우젠", LocalDate.of(2013, Month.SEPTEMBER, 30))
+
+        //then
+        assertEquals(book, expectedBook)
+    }
 }
