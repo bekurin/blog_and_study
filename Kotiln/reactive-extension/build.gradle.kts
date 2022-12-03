9a96b8c05f9d42dbb1d9ca8d32084fff2ff8927a
chungchung <bekurin17@gmail.com>
2022-12-03 12:08:59 +0900

    Feat: RxJava 리액티브 프로그래밍 kotlin 실습 프로젝트

---
+++ Kotiln/reactive-extension/build.gradle.kts
@@ -0,0 +1,25 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.7.10"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.reactivex.rxjava2:rxjava:2.2.6")
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
---
+++ Kotiln/reactive-extension/settings.gradle.kts
@@ -0,0 +1,3 @@
+
+rootProject.name = "reactive-extension"
+
