c8d45361a5fff3b4d1ac119d253ef30323aae9a7
chungchung <bekurin17@gmail.com>
2022-12-27 20:25:37 +0900

    Feat: 코틀린을 다루는 기술 3장 연습문제 8~11

--- Kotiln/kotlin_master/src/main/kotlin/chapter03/listing1/Solutions.kt
+++ Kotiln/kotlin_master/src/main/kotlin/chapter03/listing1/Solutions.kt
@@ -1,5 +1,7 @@
 package chapter03.listing1
 
+import org.jetbrains.annotations.TestOnly
+
 
 fun exercise1(f: (Int) -> Int, g: (Int) -> Int): (Int) -> Int = { data ->
     f(g(data))
@@ -22,3 +24,28 @@ val exercise4V3: (IntUnaryOp) -> (IntUnaryOp) -> IntUnaryOp =
 fun <T, U, V> exercise5(): ((U) -> V) -> ((T) -> U) -> (T) -> V = { f -> { g -> { x -> f(g(x)) } } }
 
 fun <A, B, C> exercise7(a: A, f: (A) -> (B) -> C): (B) -> C = f(a)
+
+fun <A, B, C> exercise8(b: B, f: (A) -> (B) -> C): (A) -> C = { a: A -> f(a)(b) }
+
+fun <A, B, C, D> exercise9() =
+    { a: A ->
+        { b: B ->
+            { c: C ->
+                { d: D ->
+                    "$a, $b, $c, $d"
+                }
+            }
+        }
+    }
+
+fun <A, B, C> exercise10(f: (A, B) -> C): (A) -> (B) -> C =
+    { a ->
+        { b -> f(a, b) }
+    }
+
+fun <A, B, C> exercise11(f: (A) -> (B) -> C): (B) -> (A) -> (C) =
+    { b -> { a -> f(a)(b) } }
+
+
+fun main() {
+}
