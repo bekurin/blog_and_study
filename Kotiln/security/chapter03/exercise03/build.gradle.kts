22df93f76308f7d48385094d9383b76e68da9f4c
chungchung <bekurin17@gmail.com>
2023-01-13 17:37:32 +0900

    Feat: 3장 연습문제 3번 프로젝트

---
+++ Kotiln/security/chapter03/exercise03/build.gradle.kts
@@ -0,0 +1,5 @@
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-ldap")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("com.unboundid:unboundid-ldapsdk")
+}
---
+++ Kotiln/security/chapter03/exercise03/src/main/kotlin/com/example/exercise03/Exercise03Application.kt
@@ -0,0 +1,11 @@
+package com.example.exercise03
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Exercise03Application
+
+fun main(args: Array<String>) {
+    runApplication<Exercise03Application>(*args)
+}
