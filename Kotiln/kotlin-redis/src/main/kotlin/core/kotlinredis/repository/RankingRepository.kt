ec9478b44a12691ce6ccc39844406e8c1280babb
chungchung <bekurin17@gmail.com>
2023-01-15 09:49:25 +0900

    Feat: Ranking 서비스 작성

--- Kotiln/kotlin-redis/src/main/kotlin/core/kotlinredis/repository/RankingRepository.kt
+++ Kotiln/kotlin-redis/src/main/kotlin/core/kotlinredis/repository/RankingRepository.kt
@@ -4,9 +4,11 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import core.kotlinredis.repository.RedisKey.RANKING
 import jakarta.annotation.PostConstruct
 import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.core.ScanOptions
 import org.springframework.data.redis.core.ZSetOperations
 import org.springframework.stereotype.Component
 import org.springframework.stereotype.Repository
+import org.springframework.stereotype.Service
 
 enum class RedisKey(val key: String, private val description: String) {
     RANKING("RANKING", "랭킹 기능 구현을 위한 Redis 키 값")
@@ -35,12 +37,9 @@ class PostConstruct(
 ) {
     @PostConstruct
     fun init() {
-        val entity = Member("1", "Bob", Point(1234.0))
-        rankingRepository.save(entity)
+        rankingRepository.save(Member("1", "Bob", Point(1234.0)))
         rankingRepository.save(Member("2", "Sam", Point(234.0)))
         rankingRepository.save(Member("3", "John", Point(34.0)))
-        val update = entity.update(point = Point(12345.0))
-        rankingRepository.update(entity, update)
     }
 }
 
@@ -56,14 +55,18 @@ class RankingRepository(
     }
 
     fun findAllByScoreRange(range: LongRange): List<Member> {
-        val result = rankingSet.range(RANKING.key, range.first, range.last) ?: return listOf<Member>()
+        val result = rankingSet.reverseRange(RANKING.key, range.first, range.last) ?: listOf()
         return result.map { item ->
             objectMapper.readValue(item, Member::class.java)
         }.toList()
     }
 
-    fun findRankByEntity(entity: Member): Long? {
+    fun findByEntity(entity: Member): Long? {
         return rankingSet.reverseRank(RANKING.key, objectMapper.writeValueAsString(entity))
+    }
+
+    fun findAllByRanking(count: Long) {
+        rankingSet.scan(RANKING.key, ScanOptions.scanOptions().count(count).build())
     }
 
     fun update(origin: Member, updated: Member) {
@@ -71,7 +74,40 @@ class RankingRepository(
         save(updated)
     }
 
-    fun delete(entity: Member) {
-        val count = rankingSet.remove(RANKING.key, objectMapper.writeValueAsString(entity))
+    fun delete(entity: Member): Long? {
+        return rankingSet.remove(RANKING.key, objectMapper.writeValueAsString(entity))
+    }
+}
+
+@Service
+class RankingService(
+    private val rankingRepository: RankingRepository,
+) {
+    fun save(entity: Member): Member {
+        rankingRepository.save(entity)
+        return entity
+    }
+
+    fun findAllByScoreRange(start: Long, end: Long): List<Member> {
+        return rankingRepository.findAllByScoreRange(LongRange(start, end))
+    }
+
+    fun findByEntity(entity: Member): Long {
+        return rankingRepository.findByEntity(entity) ?: throw RuntimeException("${entity.id}를 찾을 수 없습니다")
+    }
+
+    fun findAllByRanking(count: Long) {
+        return rankingRepository.findAllByRanking(count)
+    }
+
+    fun update(entity: Member, score: Double) {
+        return rankingRepository.update(
+            entity,
+            entity.update(entity.nickname, Point(score))
+        )
+    }
+
+    fun delete(entity: Member): Long {
+        return rankingRepository.delete(entity) ?: throw RuntimeException("${entity.id}를 가진 회원을 찾지 못했습니다")
     }
 }
