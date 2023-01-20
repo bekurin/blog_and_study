b284a1ed187851ba3d38c4cca9fe9bec1efc464a
chungchung <bekurin17@gmail.com>
2023-01-20 22:56:34 +0900

    feat: AuthenticationEntryPoint 처리

---
+++ Kotiln/security/chapter05/exercise02/src/main/kotlin/com/example/exercise02/Exercise02Application.kt
@@ -0,0 +1,11 @@
+package com.example.exercise02
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Exercise02Application
+
+fun main(args: Array<String>) {
+    runApplication<Exercise02Application>(*args)
+}
