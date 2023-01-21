01eaf06882d894d2bb963690614605d338c83749
chungchung <bekurin17@gmail.com>
2023-01-21 21:39:46 +0900

    feat: 11장 실전: 책임의 분리 프로젝트

---
+++ Kotiln/security/chapter11/src/test/kotlin/com/example/chapter11/Chapter11ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.chapter11
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Chapter11ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
