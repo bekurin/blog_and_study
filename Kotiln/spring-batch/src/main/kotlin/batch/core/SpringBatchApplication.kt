b5eb8a20134cea69220166ee478efe6d383705d8
chungchung <bekurin17@gmail.com>
2023-03-10 22:44:41 +0900

    init spring batch project

---
+++ Kotiln/spring-batch/src/main/kotlin/batch/core/SpringBatchApplication.kt
@@ -0,0 +1,11 @@
+package batch.core
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBatchApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBatchApplication>(*args)
+}
---
+++ Kotiln/spring-batch/src/test/kotlin/batch/core/SpringBatchApplicationTests.kt
@@ -0,0 +1,13 @@
+package batch.core
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBatchApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
