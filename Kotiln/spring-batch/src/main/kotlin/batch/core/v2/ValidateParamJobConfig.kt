43c1ac2d1550e97d014b593ad12c165c9722ccfc
chungchung <bekurin17@gmail.com>
2023-03-15 21:15:23 +0900

    add JobScope annotation and line break

--- Kotiln/spring-batch/src/main/kotlin/batch/core/v2/ValidateParamJobConfig.kt
+++ Kotiln/spring-batch/src/main/kotlin/batch/core/v2/ValidateParamJobConfig.kt
@@ -4,26 +4,22 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.batch.core.Job
 import org.springframework.batch.core.Step
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
 import org.springframework.batch.core.configuration.annotation.JobScope
 import org.springframework.batch.core.configuration.annotation.StepScope
 import org.springframework.batch.core.configuration.support.DefaultBatchConfiguration
 import org.springframework.batch.core.job.CompositeJobParametersValidator
 import org.springframework.batch.core.job.builder.JobBuilder
 import org.springframework.batch.core.launch.support.RunIdIncrementer
-import org.springframework.batch.core.repository.JobRepository
 import org.springframework.batch.core.step.builder.StepBuilder
 import org.springframework.batch.core.step.tasklet.Tasklet
 import org.springframework.batch.repeat.RepeatStatus
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.transaction.PlatformTransactionManager
 
 
 @Configuration
-class ValidateParamJobConfig: DefaultBatchConfiguration() {
+class ValidateParamJobConfig : DefaultBatchConfiguration() {
     @Value("\${spring.batch.job.name}")
     private lateinit var jobName: String
 
@@ -45,6 +41,7 @@ class ValidateParamJobConfig: DefaultBatchConfiguration() {
         return validators
     }
 
+    @JobScope
     @Bean
     fun validateParamStep(validateParamTasklet: Tasklet): Step {
         log.info("step start...")
