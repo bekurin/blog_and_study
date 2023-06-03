7e18e78875e9227a5ade4456d53620c63c6f144a
bekurin <bekurin17@gmail.com>
2023-06-03 18:29:29 +0900

    feat: locust 수정

--- Python/locust/locust.py
+++ Python/locust/locust.py
@@ -1,14 +1,21 @@
 from locust import TaskSet, task, HttpUser, between
-
+import random
 
 class User(HttpUser):
-    wait_time = 0
+    wait_time = between(0, 0)
 
     @task
-    def index(self):
-        self.client.get("/")
+    def path_variable(self):
+        id = random.randint(1, 10000)
+        self.client.get("/validate/path-variable/" + str(id))
+        
+    @task
+    def request_params(self):
+        id = random.randint(1, 10000)
+        self.client.get("/validate/request-params?id=" + str(id))        
 
-    @task(3)
-    def login(self):
+    @task
+    def request_body(self):
+        id = random.randint(1, 10000)
         self.client.post(
-            "/login", json={"username": "testuser", "password": "1111"})
+            "/validate/request-body", json={"id": id, "message": "fixed message"})
