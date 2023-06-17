72477e7bfb07805c0c813af26c19453d644fbcce
bekurin <bekurin17@gmail.com>
2023-06-17 20:29:06 +0900

    feat: add jotai

--- Project/blog/frontend/src/pages/post/PostList.tsx
+++ Project/blog/frontend/src/pages/post/PostList.tsx
@@ -1,11 +1,8 @@
-import usePostStore from "./stores"
 import {DataTable} from "primereact/datatable"
 import { Column } from "primereact/column"
 
 const PostList = () => {
-    const {posts} = usePostStore()
-
-    return <DataTable value={posts?.contents}>
+    return <DataTable>
         <Column>haha</Column>
     </DataTable>
 }
--- Project/blog/frontend/src/pages/post/PostSearch.tsx
+++ Project/blog/frontend/src/pages/post/PostSearch.tsx
@@ -1,20 +1,21 @@
-import usePostStore from "./stores"
 import {InputText} from "primereact/inputtext"
 import {Button} from "primereact/button"
 import { FormEvent } from "react"
 
 const PostSearch = () => {
-    const {updateQueryParams} = usePostStore()
 
     const handleSubmitAction = (event: FormEvent) => {
         event.preventDefault()
-        console.log(event.currentTarget)
+        console.log(event.currentTarget.title.value)
     }
 
     return (
-        <form onSubmit={handleSubmitAction}>
-            <input name="title" type="text" placeholder="제목을 입력해주세요" />
-            <Button type="submit">검색하기</Button>
+        <form className="" onSubmit={handleSubmitAction}>
+            <div className="form-group">
+                <label id="title">제목</label>
+                <input className="form-control" id="title" name="title" type="text" placeholder="제목을 입력해주세요" />
+            </div>
+            <Button className="btn btn-primary" type="submit">검색하기</Button>
         </form>
         )
 }
