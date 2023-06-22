ae60af4c2c28ec8e313188a7a043bce4e6326750
bekurin <bekurin17@gmail.com>
2023-06-22 21:10:51 +0900

    feat: declare parameter spec

--- Project/blog/backend/src/main/kotlin/com/blog/backend/controller/CommentControllerSpec.kt
+++ Project/blog/backend/src/main/kotlin/com/blog/backend/controller/CommentControllerSpec.kt
@@ -4,14 +4,30 @@ import com.blog.backend.controller.dto.CommentDto
 import com.blog.backend.controller.dto.CreateCommentDto
 import com.blog.backend.controller.dto.PageDto
 import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.Parameters
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.parameters.RequestBody
 
 interface CommentControllerSpec {
     @Operation(summary = "댓글 리스트 조회", description = "글 ID에 등록되어 있는 댓글을 리스트로 조회한다.")
+    @Parameters(
+        Parameter(name = "page", description = "페이지 번호", required = true),
+        Parameter(name = "size", description = "페이지 아이템 개수", required = true),
+        Parameter(name = "postId", description = "글 ID", required = true),
+    )
     fun findPagedCommentByPostId(postId: Long, page: Int, size: Int): PageDto<CommentDto>
 
     @Operation(summary = "댓글 생성", description = "작성자, 댓글 내용을 입력 받아 댓글을 생성한다.")
+    @RequestBody(
+        description = "댓글 생성 요청", required = true, content = [
+            Content(schema = Schema(implementation = CreateCommentDto::class))
+        ]
+    )
     fun createComment(dto: CreateCommentDto): CommentDto
 
     @Operation(summary = "댓글 삭제", description = "댓글 ID를 사용하여 댓글을 삭제한다.")
+    @Parameter(name = "id", description = "댓글 ID", required = true)
     fun deleteById(id: Long)
 }
--- Project/blog/backend/src/main/kotlin/com/blog/backend/controller/PostControllerSpec.kt
+++ Project/blog/backend/src/main/kotlin/com/blog/backend/controller/PostControllerSpec.kt
@@ -3,76 +3,58 @@ package com.blog.backend.controller
 import com.blog.backend.controller.dto.CreatePostDto
 import com.blog.backend.controller.dto.PageDto
 import com.blog.backend.controller.dto.PostDto
-import com.blog.backend.exception.ErrorResponse
 import io.swagger.v3.oas.annotations.Operation
-import io.swagger.v3.oas.annotations.media.ArraySchema
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.Parameters
 import io.swagger.v3.oas.annotations.media.Content
 import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.parameters.RequestBody
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
-import org.springframework.http.MediaType
 
 interface PostControllerSpec {
     @Operation(summary = "글 리스트 조회", description = "모든 글 리스트 조회한다.")
+    @Parameters(
+        Parameter(name = "page", description = "페이지 번호", required = true),
+        Parameter(name = "size", description = "페이지 아이템 개수", required = true),
+        Parameter(name = "title", description = "글 제목"),
+    )
     @ApiResponses(
         value = [
-            ApiResponse(description = "글 리스트 조회", responseCode = "200"),
-            ApiResponse(
-                description = "유효하지 않은 파라미터", responseCode = "400", content = [
-                    Content(
-                        mediaType = MediaType.APPLICATION_JSON_VALUE,
-                        array = ArraySchema(schema = Schema(implementation = ErrorResponse::class))
-                    )
-                ]
-            ),
+            ApiResponse(description = "글 리스트 조회", responseCode = "200")
         ]
     )
     fun findPagedPost(page: Int, size: Int, title: String?): PageDto<PostDto>
 
     @Operation(summary = "글 단건 조회", description = "글 ID를 사용하여 단건 조회한다.")
+    @Parameters(
+        Parameter(name = "id", description = "글 ID", required = true),
+    )
     @ApiResponses(
         value = [
             ApiResponse(description = "글 단건 조회", responseCode = "200"),
-            ApiResponse(
-                description = "유효하지 않은 파라미터", responseCode = "400", content = [
-                    Content(
-                        mediaType = MediaType.APPLICATION_JSON_VALUE,
-                        array = ArraySchema(schema = Schema(implementation = ErrorResponse::class))
-                    )
-                ]
-            ),
         ]
     )
     fun findPostById(id: Long): PostDto
 
     @Operation(summary = "글 생성", description = "글 제목, 내용을 입력받아 글을 생성한다.")
+    @RequestBody(
+        description = "글 생성 요청", required = true, content = [
+            Content(schema = Schema(implementation = CreatePostDto::class))
+        ]
+    )
     @ApiResponses(
         value = [
-            ApiResponse(description = "글 생성", responseCode = "201"),
-            ApiResponse(
-                description = "유효하지 않은 파라미터", responseCode = "400", content = [
-                    Content(
-                        mediaType = MediaType.APPLICATION_JSON_VALUE,
-                        array = ArraySchema(schema = Schema(implementation = ErrorResponse::class))
-                    )
-                ]
-            ),
+            ApiResponse(description = "글 생성", responseCode = "201")
         ]
     )
     fun createPost(request: CreatePostDto): PostDto
 
     @Operation(summary = "글 삭제", description = "글 ID를 사용하여 글을 삭제한다.")
+    @Parameter(name = "id", description = "글 ID", required = true)
     @ApiResponses(
         value = [
             ApiResponse(description = "글 삭제", responseCode = "204"),
-            ApiResponse(
-                description = "유효하지 않은 파라미터", responseCode = "400", content = [
-                    Content(
-                        mediaType = MediaType.APPLICATION_JSON_VALUE,
-                        array = ArraySchema(schema = Schema(implementation = ErrorResponse::class))
-                    )
-                ]
-            ),
         ]
     )
     fun deleteById(id: Long)
