b60ed48abd7349a6c0ff822dd25d16cbbfe833b4
bekurin <bekurin17@gmail.com>
2023-09-04 20:52:43 +0900

    feat: init google spread sheet api project

---
+++ Kotiln/google-spread-sheet/src/main/kotlin/core/googlespreadsheet/GoogleSpreadSheetApplication.kt
@@ -0,0 +1,11 @@
+package core.googlespreadsheet
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GoogleSpreadSheetApplication
+
+fun main(args: Array<String>) {
+    runApplication<GoogleSpreadSheetApplication>(*args)
+}
---
+++ Kotiln/google-spread-sheet/src/test/kotlin/core/googlespreadsheet/GoogleSpreadSheetApplicationTests.kt
@@ -0,0 +1,13 @@
+package core.googlespreadsheet
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GoogleSpreadSheetApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
