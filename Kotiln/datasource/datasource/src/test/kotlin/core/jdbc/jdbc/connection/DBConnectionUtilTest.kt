68db674cd4137d40a7988a28fa3e7f8f836adfb1
bekurin <bekurin17@gmail.com>
2023-12-02 13:24:40 +0900

    feat: driverManager, driverManagerDataSource, HikariDataSource 연결 테스트

--- Kotiln/datasource/datasource/src/test/kotlin/core/jdbc/jdbc/connection/DBConnectionUtilTest.kt
+++ Kotiln/datasource/datasource/src/test/kotlin/core/jdbc/jdbc/connection/DBConnectionUtilTest.kt
@@ -1,11 +1,14 @@
 package core.jdbc.jdbc.connection
 
+import com.zaxxer.hikari.HikariDataSource
 import core.jdbc.connection.DBConnectionConstant.PASSWORD
 import core.jdbc.connection.DBConnectionConstant.URL
 import core.jdbc.connection.DBConnectionConstant.USERNAME
 import org.junit.jupiter.api.Test
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.jdbc.datasource.DriverManagerDataSource
+import java.sql.Connection
 import java.sql.DriverManager
 
 class DBConnectionUtilTest {
@@ -13,10 +16,45 @@ class DBConnectionUtilTest {
 
     @Test
     fun `DriverManager로 커넥션을 생성하면 매번 새로운 연결을 획득한다`() {
+        // 매번 설정 파일을 파라미터로 넘겨줘야한다.
         val conn1 = DriverManager.getConnection(URL, USERNAME, PASSWORD)
         val conn2 = DriverManager.getConnection(URL, USERNAME, PASSWORD)
+        loggingConnections(conn1, conn2)
+    }
 
-        log.info("connection={}, class={}", conn1, conn1::class.java)
-        log.info("connection={}, class={}", conn2, conn2::class.java)
+    @Test
+    fun `DriverManagerDataSource를 사용하여 DriverManager를 Datasource로 변경할 수 있다`() {
+        // 설정과 연결이 분리되어 있다.
+        val datasource = DriverManagerDataSource(URL, USERNAME, PASSWORD)
+
+        val conn1 = datasource.connection
+        val conn2 = datasource.connection
+        loggingConnections(conn1, conn2)
+    }
+
+    @Test
+    fun `커넥션 풀을 사용하면 기존에 생성된 연결을 가져와서 사용한다`() {
+        val givenMaximumPoolSize = 8
+        val datasource = HikariDataSource().apply {
+            jdbcUrl = URL
+            username = USERNAME
+            password = PASSWORD
+            maximumPoolSize = 8
+            connectionTimeout = 0
+            poolName = "HANGMAN"
+        }
+
+        val connections = (1..givenMaximumPoolSize).map {
+            datasource.connection
+        }.toTypedArray()
+
+        loggingConnections(*connections)
+        Thread.sleep(3000L)
+    }
+
+    private fun loggingConnections(vararg connections: Connection) {
+        connections.map { connection ->
+            log.info("connection={}, class={}", connection, connection::class.java)
+        }
     }
 }
