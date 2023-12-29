d44b53b5fff10d772691b20a80afe21d4a73d2b8
bekurin <bekurin17@gmail.com>
2023-12-29 20:16:58 +0900

    feat: create redis factory connection

--- crud/redis/src/main/kotlin/curd/redis/RedisConfig.kt
+++ crud/redis/src/main/kotlin/curd/redis/RedisConfig.kt
@@ -3,13 +3,24 @@ package curd.redis
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import org.springframework.data.redis.core.RedisTemplate
-import org.springframework.data.redis.core.StringRedisTemplate
+import org.springframework.data.redis.serializer.StringRedisSerializer
 
 @Configuration
 class RedisConfig {
     @Bean
     fun redisTemplate(redisConnectionFactory: RedisConnectionFactory): RedisTemplate<String, String> {
-        return StringRedisTemplate()
+        val stringSerializer = StringRedisSerializer()
+        val redisTemplate = RedisTemplate<String, String>().apply {
+            setDefaultSerializer(stringSerializer)
+        }
+        redisTemplate.connectionFactory = redisConnectionFactory()
+        return redisTemplate
+    }
+
+    @Bean
+    fun redisConnectionFactory(): LettuceConnectionFactory {
+        return LettuceConnectionFactory("localhost", 6379)
     }
 }
