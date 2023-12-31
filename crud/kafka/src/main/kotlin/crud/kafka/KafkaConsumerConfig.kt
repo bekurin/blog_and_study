414a9be9ae26740b1a58a0fe35eec298a7af747a
bekurin <bekurin17@gmail.com>
2023-12-31 19:10:23 +0900

    feat: create retry error handling when occurred exception while processing message

--- crud/kafka/src/main/kotlin/crud/kafka/KafkaConsumerConfig.kt
+++ crud/kafka/src/main/kotlin/crud/kafka/KafkaConsumerConfig.kt
@@ -2,15 +2,21 @@ package crud.kafka
 
 import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.common.serialization.StringDeserializer
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
 import org.springframework.kafka.core.ConsumerFactory
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory
+import org.springframework.kafka.listener.DefaultErrorHandler
+import org.springframework.web.client.HttpServerErrorException.InternalServerError
 
 
 @Configuration
 class KafkaConsumerConfig {
+    private val log = LoggerFactory.getLogger(KafkaConsumerConfig::class.java)
+
     @Bean
     fun consumerFactory(): ConsumerFactory<String, String> {
         val config: MutableMap<String, Any> = HashMap()
@@ -25,6 +31,15 @@ class KafkaConsumerConfig {
     fun kafkaListenerContainerFactory(): ConcurrentKafkaListenerContainerFactory<String, String> {
         val factory = ConcurrentKafkaListenerContainerFactory<String, String>()
         factory.consumerFactory = consumerFactory()
+        factory.setCommonErrorHandler(handlerError())
         return factory
+    }
+
+    fun handlerError(): DefaultErrorHandler {
+        val defaultErrorHandler = DefaultErrorHandler { consumerRecord, exception ->
+            log.error("${consumerRecord.topic()} 처리 중에 예외가 발생했습니다.", exception)
+        }
+        defaultErrorHandler.addRetryableExceptions(InternalServerError::class.java)
+        return defaultErrorHandler
     }
 }
--- crud/kafka/src/main/kotlin/crud/kafka/KafkaConsumerService.kt
+++ crud/kafka/src/main/kotlin/crud/kafka/KafkaConsumerService.kt
@@ -1,13 +1,17 @@
 package crud.kafka
 
 import crud.kafka.Constant.TOPIC
+import org.slf4j.LoggerFactory
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.stereotype.Service
 
 @Service
 class KafkaConsumerService {
-    @KafkaListener(topics = [TOPIC], groupId = "myGroup")
+    private val log = LoggerFactory.getLogger(KafkaConsumerService::class.java)
+
+    @KafkaListener(topics = [TOPIC], groupId = "crudKafka")
     fun listen(message: String) {
-        println("Received Message: $message")
+        // throw RuntimeException("예외 발생")
+        log.info("Received Message: $message")
     }
 }
