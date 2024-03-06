26b4f83309844ca4f028428a2a2be1cce48fa271
chungchung <bekurin17@gmail.com>
2024-03-06 13:23:44 +0900

    feat: next token 생성 및 테스트 코드 보완

--- Go/interpreter/lexer/lexer_test.go
+++ Go/interpreter/lexer/lexer_test.go
@@ -1,24 +1,58 @@
 package lexer
 
 import (
-	"testing"
 	"interpreter/token"
+	"testing"
 )
 
 func TestNextToken(t *testing.T) {
-	input := `=+(){},;`
+	input := `let five = 5;
+let ten = 10;
+let add = fn(x, y) {
+	x + y;
+};
+let result = add(five, ten);
+`
 
 	tests := []struct {
 		expectedType    token.TokenType
 		expectedLiteral string
 	}{
+		{token.LET, "let"},
+		{token.IDENT, "five"},
 		{token.ASSIGN, "="},
-		{token.PLUS, "+"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "ten"},
+		{token.ASSIGN, "="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "add"},
+		{token.ASSIGN, "="},
+		{token.FUNCTION, "fn"},
 		{token.LPAREN, "("},
+		{token.IDENT, "x"},
+		{token.COMMA, ","},
+		{token.IDENT, "y"},
 		{token.RPAREN, ")"},
 		{token.LBRACE, "{"},
-		{token.RBRACE, "}"},
+		{token.IDENT, "x"},
+		{token.PLUS, "+"},
+		{token.IDENT, "y"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "="},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "result"},
+		{token.ASSIGN, "="},
+		{token.IDENT, "add"},
+		{token.LPAREN, "("},
+		{token.IDENT, "five"},
 		{token.COMMA, ","},
+		{token.IDENT, "ten"},
+		{token.RPAREN, ")"},
 		{token.SEMICOLON, ";"},
 		{token.EOF, ""},
 	}
@@ -26,12 +60,12 @@ func TestNextToken(t *testing.T) {
 	l := New(input)
 
 	for i, tt := range tests {
-		token := l.NextToken()
-		if token.Type != tt.expectedType {
-			t.Fatalf("tests[%d] - tokentype wrong. expectd=%q, got=%q", i, tt.expectedType, token.type)
+		tok := l.NextToken()
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - tokentype wrong. expectd=%q, got=%q", i, tt.expectedType, tok.Type)
 		}
-		if token.Literal != tt.expectedLiteral {
-			t.Fatalf("tests[%d] - literal wrong. expectd=%q, got=%q", i, tt.expectedLiteral, token.Literal)
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expectd=%q, got=%q", i, tt.expectedLiteral, tok.Literal)
 		}
 	}
 }
