ae8a46d8b2a97bf8482031744eb422e859bed29c
bekurin <bekurin17@gmail.com>
2024-05-06 17:05:38 +0900

    feat: separate the component into method

--- Flutter/daily_cats_app/lib/screen/upload_screen.dart
+++ Flutter/daily_cats_app/lib/screen/upload_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class UploadScreen extends StatefulWidget {
   const UploadScreen({super.key});
@@ -8,6 +11,17 @@ class UploadScreen extends StatefulWidget {
 }
 
 class _UploadScreenState extends State<UploadScreen> {
+  String? imagePath;
+
+  Future<String?> selectImage() async {
+    final picker = ImagePicker();
+    XFile? pickImage = await picker.pickImage(source: ImageSource.gallery);
+    if (pickImage == null) {
+      return null;
+    }
+    return pickImage.path;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -17,47 +31,47 @@ class _UploadScreenState extends State<UploadScreen> {
       child: SafeArea(
         bottom: false,
         child: Scaffold(
-          appBar: AppBar(
-            title: const Text("사진 업로드"),
-            leading: IconButton(
-              icon: const Text("취소"),
-              padding: EdgeInsets.zero,
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-            actions: <Widget>[
-              IconButton(
-                icon: const Text("저장"),
-                padding: EdgeInsets.zero,
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-              )
-            ],
-          ),
+          appBar: getAppBar(context),
           body: Container(
             color: Colors.white,
             child: Center(
-              child: Column(
-                children: [
-                  Container(
-                    width: 300.0,
-                    height: 300.0,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(5.0),
-                      border: Border.all(
-                          width: 0.5, color: const Color(0xFFAAAAAA)),
-                    ),
-                    child: const Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(Icons.upload, size: 50.0),
-                        Text("고양이 사진 업로드")
-                      ],
-                    ),
-                  )
-                ],
+              child: SingleChildScrollView(
+                child: GestureDetector(
+                  onTap: () {
+                    selectImage().then((String? path) {
+                      if (path == null) {
+                        return;
+                      }
+                      setState(() {
+                        imagePath = path;
+                      });
+                    });
+                  },
+                  behavior: HitTestBehavior.translucent,
+                  child: Column(
+                    children: [
+                      Container(
+                        width: 300.0,
+                        height: 300.0,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5.0),
+                          border: Border.all(
+                              width: 0.5, color: const Color(0xFFAAAAAA)),
+                        ),
+                        child: imagePath != null
+                            ? Image.file(File(imagePath!),
+                                width: 200.0, height: 200.0)
+                            : const Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Icon(Icons.upload, size: 50.0),
+                                  Text("고양이 사진 업로드")
+                                ],
+                              ),
+                      )
+                    ],
+                  ),
+                ),
               ),
             ),
           ),
@@ -65,4 +79,26 @@ class _UploadScreenState extends State<UploadScreen> {
       ),
     );
   }
+}
+
+AppBar getAppBar(BuildContext context) {
+  return AppBar(
+    title: const Text("사진 업로드"),
+    leading: IconButton(
+      icon: const Text("취소"),
+      padding: EdgeInsets.zero,
+      onPressed: () {
+        Navigator.of(context).pop();
+      },
+    ),
+    actions: <Widget>[
+      IconButton(
+        icon: const Text("저장"),
+        padding: EdgeInsets.zero,
+        onPressed: () {
+          Navigator.of(context).pop();
+        },
+      )
+    ],
+  );
 }
