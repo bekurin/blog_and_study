9565af588225c618ed2bdfb40857fe6c142df21c
bekurin <bekurin17@gmail.com>
2024-05-13 22:17:53 +0900

    feat: implement passwordEncoder

--- Kotiln/authentication/build.gradle.kts
+++ Kotiln/authentication/build.gradle.kts
@@ -27,6 +27,8 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
--- Kotiln/authentication/src/main/kotlin/backend/authentication/config/SecurityConfig.kt
+++ Kotiln/authentication/src/main/kotlin/backend/authentication/config/SecurityConfig.kt
@@ -2,11 +2,14 @@ package backend.authentication.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.core.userdetails.User
 import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.crypto.factory.PasswordEncoderFactories
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder
 import org.springframework.security.provisioning.InMemoryUserDetailsManager
 import org.springframework.security.web.SecurityFilterChain
 
@@ -32,5 +35,19 @@ class SecurityConfig {
     fun userDetailsService(): UserDetailsService {
         val userDetails = User.withUsername("user").password("123").roles("USER").build()
         return InMemoryUserDetailsManager(userDetails)
+    }
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder {
+        // Default bcryptPasswordEncoder
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder()
+    }
+
+    // @Bean 
+    fun customPasswordEncoder(): PasswordEncoder {
+        val encodingId = "pbkdf2"
+        val encoderMap = mapOf(encodingId to Pbkdf2PasswordEncoder.defaultsForSpringSecurity_v5_8())
+        val delegatingPasswordEncoder = DelegatingPasswordEncoder(encodingId, encoderMap)
+        return delegatingPasswordEncoder
     }
 }
