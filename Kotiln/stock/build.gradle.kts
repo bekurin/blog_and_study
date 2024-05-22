6989711c514c3ef4f7074d58e169085392225db1
bekurin <bekurin17@gmail.com>
2024-05-22 23:28:39 +0900

    feat: upgrade redisson-spring-boot-starter version to 3.30.0

--- Kotiln/stock/build.gradle.kts
+++ Kotiln/stock/build.gradle.kts
@@ -23,7 +23,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.redisson:redisson-spring-boot-starter:3.17.4")
+    implementation("org.redisson:redisson-spring-boot-starter:3.30.0")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     runtimeOnly("com.mysql:mysql-connector-j")
