e774b4921d4ef623936b8ec0cc6a59d6bdbac372
bekurin <bekurin17@gmail.com>
2024-08-04 13:19:46 +0900

    feat: init MinesweeperGame project

---
+++ Java/minesweeper/src/test/java/core/minesweeper/MinesweeperApplicationTests.java
@@ -0,0 +1,13 @@
+package core.minesweeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinesweeperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
