1a005edc7ae02f94d48653ed897106d454139401
chungchung <hangjastar@gmail.com>
2024-10-01 18:36:31 +0900

    feat: config setting to server using toml and flag

--- Go/demo-scrapping/cmd/main.go
+++ Go/demo-scrapping/cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"demo-scrapping/config"
 	"flag"
 	"fmt"
 )
@@ -10,6 +11,6 @@ var pathFlag = flag.String("config", "./config.toml", "set toml path")
 func main() {
 	flag.Parse()
 
-	fmt.Println(*pathFlag)
-	fmt.Println("hello world")
+	config := config.NewConfig(*pathFlag)
+	fmt.Println(config)
 }
