97d7a04987c9da7f716311a903cfc7ca70c1ceee
chungchung <hangjastar@gmail.com>
2024-10-05 17:42:01 +0900

    feat: create skeleton of demo-scarpping project

---
+++ Go/demo-scrapping/network/util.go
@@ -0,0 +1,55 @@
+package network
+
+import (
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+type API_REQUEST_METHOD uint8
+
+const (
+	GET API_REQUEST_METHOD = iota
+	POST
+	PUT
+	PATCH
+	UPDATE
+	DELETE
+)
+
+type header struct {
+	result int    `json:"result"`
+	data   string `json:"data"`
+}
+
+type response struct {
+	*header
+	result interface{} `json:"result"`
+}
+
+func res(context *gin.Context, code int, res interface{}, data ...string) {
+	context.JSON(code, &response{
+		header: &header{
+			result: code,
+			data:   strings.Join(data, " ,"),
+		},
+		result: res,
+	})
+}
+
+func (network *Network) register(path string, method API_REQUEST_METHOD, handlerFunc ...gin.HandlerFunc) gin.IRoutes {
+	switch method {
+	case GET:
+		return network.engine.GET(path, handlerFunc...)
+	case POST:
+		return network.engine.POST(path, handlerFunc...)
+	case PUT:
+		return network.engine.PUT(path, handlerFunc...)
+	case PATCH:
+		return network.engine.PATCH(path, handlerFunc...)
+	case DELETE:
+		return network.engine.DELETE(path, handlerFunc...)
+	default:
+		return nil
+	}
+}
