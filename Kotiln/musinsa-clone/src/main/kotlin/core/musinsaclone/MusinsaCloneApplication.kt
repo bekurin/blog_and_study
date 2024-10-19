148915e4df03b9c8b0feb4acaf076ed28208561a
chungchung <hangjastar@gmail.com>
2024-10-20 07:53:51 +0900

    create: musinsa-demo project

---
+++ Kotiln/musinsa-clone/src/main/kotlin/core/musinsaclone/MusinsaCloneApplication.kt
@@ -0,0 +1,11 @@
+package core.musinsaclone
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MusinsaCloneApplication
+
+fun main(args: Array<String>) {
+    runApplication<MusinsaCloneApplication>(*args)
+}
---
+++ Kotiln/musinsa-clone/src/test/kotlin/core/musinsaclone/MusinsaCloneApplicationTests.kt
@@ -0,0 +1,13 @@
+package core.musinsaclone
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MusinsaCloneApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
