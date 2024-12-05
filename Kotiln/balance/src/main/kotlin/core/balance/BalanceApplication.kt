a70ed9df685d63aa0068200793022249b249123a
chungchung <hangjastar@gmail.com>
2024-12-05 20:24:19 +0900

    feat: init balance project

---
+++ Kotiln/balance/src/main/kotlin/core/balance/BalanceApplication.kt
@@ -0,0 +1,11 @@
+package core.balance
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BalanceApplication
+
+fun main(args: Array<String>) {
+    runApplication<BalanceApplication>(*args)
+}
