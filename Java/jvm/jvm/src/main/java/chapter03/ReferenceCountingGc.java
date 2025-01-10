c14ebb891f2cddec18252edac1bd792ec461bde9
chungchung <hangjastar@gmail.com>
2025-01-10 20:52:47 +0900

    feat: implement ReferenceCountingGc

---
+++ Java/jvm/jvm/src/main/java/chapter03/ReferenceCountingGc.java
@@ -0,0 +1,25 @@
+package chapter03;
+
+public class ReferenceCountingGc {
+    public Object instance = null;
+    private static final int _1MB = 1024 * 1024;
+
+    private byte[] bigSize = new byte[2 * _1MB];
+
+    public static void main(String[] args) {
+        testGc();
+    }
+
+    private static void testGc() {
+        ReferenceCountingGc objectA = new ReferenceCountingGc();
+        ReferenceCountingGc objectB = new ReferenceCountingGc();
+
+        objectA.instance = objectB;
+        objectB.instance = objectA;
+
+        objectA = null;
+        objectB = null;
+
+        System.gc();
+    }
+}
