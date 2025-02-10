946a44c535227adf569080ae622c56dd6700d536
chungchung <hangjastar@gmail.com>
2025-02-10 20:07:50 +0900

    feat: implement HomeController

---
+++ Java/rabbit-mq/src/main/java/core/rabbitmq/topic3pubsub/controller/HomeController.java
@@ -0,0 +1,15 @@
+package core.rabbitmq.topic3pubsub.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/home")
+    public String home(Model model) {
+        model.addAttribute("message", "Welcome to RabbitMQ!");
+        return "home";
+    }
+}
