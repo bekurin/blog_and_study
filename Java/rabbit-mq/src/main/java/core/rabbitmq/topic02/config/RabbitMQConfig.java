22ba69abed5d2a7351c6156385b27156d0f87a49
chungchung <hangjastar@gmail.com>
2025-02-10 19:54:46 +0900

    chore: disable topic2

--- Java/rabbit-mq/src/main/java/core/rabbitmq/topic02/config/RabbitMQConfig.java
+++ Java/rabbit-mq/src/main/java/core/rabbitmq/topic02/config/RabbitMQConfig.java
@@ -10,7 +10,7 @@ import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 public class RabbitMQConfig {
     public static final String QUEUE_NAME = "WorkQueue";
 
