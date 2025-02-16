d66cd562029b7f2fa00b7262d81f70164a70ed1b
chungchung <hangjastar@gmail.com>
2025-02-16 21:58:51 +0900

    chore: disable topic4

--- Java/rabbit-mq/src/main/java/core/rabbitmq/topic4pubsub2/config/RabbitMqConfig.java
+++ Java/rabbit-mq/src/main/java/core/rabbitmq/topic4pubsub2/config/RabbitMqConfig.java
@@ -7,9 +7,8 @@ import org.springframework.amqp.core.Queue;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 public class RabbitMqConfig {
-    // step4 예제 추가
     public static final String FANOUT_EXCHANGE_FOR_NEWS = "newsExchange";
 
     public static final String JAVA_QUEUE = "javaQueue";
