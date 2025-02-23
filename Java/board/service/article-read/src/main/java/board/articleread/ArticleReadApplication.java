abc819e3312260fc3cdc1b8bab7a55d044559323
chungchung <bekurin17@gmail.com>
2025-02-23 22:10:34 +0900

    feat: initialize board project

---
+++ Java/board/service/article-read/src/main/java/board/articleread/ArticleReadApplication.java
@@ -0,0 +1,11 @@
+package board.articleread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticleReadApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ArticleReadApplication.class, args);
+    }
+}
