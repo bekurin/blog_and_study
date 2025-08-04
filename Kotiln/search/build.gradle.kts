0b40d42e7ec7bab25d815774a1e2dab6e694e5f6
chungchung <hangjastar@gmail.com>
2025-08-04 22:41:15 +0900

    feat: add pluginManagement and allProjects scope

--- Kotiln/search/build.gradle.kts
+++ Kotiln/search/build.gradle.kts
@@ -1,23 +1,12 @@
-plugins {
-	kotlin("jvm") version "2.2.0"
-	kotlin("plugin.spring") version "2.2.0"
-	id("org.springframework.boot") version "4.0.0-M1"
-	id("io.spring.dependency-management") version "1.1.7"
-}
-
-repositories {
-	mavenCentral()
+allprojects {
+	repositories {
+		mavenCentral()
+	}
+	group = "stock"
+	version = "0.0.1-SNAPSHOT"
 }
 
 subprojects {
-	apply(plugin = "org.springframework.boot")
-	apply(plugin = "io.spring.dependency-management")
-	apply(plugin = "kotlin")
-	apply(plugin = "kotlin-spring")
-
-	group = "stock"
-	version = "0.0.1-SNAPSHOT"
-
 	java {
 		toolchain {
 			languageVersion = JavaLanguageVersion.of(21)
--- Kotiln/search/settings.gradle.kts
+++ Kotiln/search/settings.gradle.kts
@@ -7,3 +7,16 @@ include("stock-consumer")
 include("stock-modules")
 include("stock-modules:database")
 include("stock-modules:web")
+
+pluginManagement {
+    repositories {
+        mavenCentral()
+    }
+    plugins {
+        kotlin("jvm") version "2.2.0"
+        kotlin("plugin.spring") version "2.2.0"
+        id("org.springframework.boot") version "4.0.0-M1"
+        id("io.spring.dependency-management") version "1.1.7"
+    }
+}
+
